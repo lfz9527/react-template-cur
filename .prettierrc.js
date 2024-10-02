00ca95fa5a0651f45c32734faea2ed9efe226499
lfz9527 <1397118980@qq.com>
2024-10-03 02:25:03 +0800

    feat: 添加统一代码规范

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+// Prettier 配置文件
+module.exports = {
+    // 不使用尾随逗号
+    trailingComma: 'none',
+    // 不使用分号
+    semi: false,
+    // 使用单引号
+    singleQuote: true,
+    // JSX 中也使用单引号
+    jsxSingleQuote: true,
+    // 使用 LF 作为行结束符
+    endOfLine: 'lf',
+    // 设置每行最大宽度为 120 字符
+    printWidth: 80,
+    // 使用 4 个空格缩进
+    tabWidth: 4,
+    // 不对 Markdown 文本进行换行
+    proseWrap: 'never',
+    // 对象字面量中的括号内不添加空格
+    bracketSpacing: false,
+    // 箭头函数参数始终使用括号
+    arrowParens: 'always',
+}
